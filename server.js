c0211d35b6c69313fce92c063f1efdf8e77fa7ba
Huiyeon Kim <gnldus59112@gmail.com>
2018-11-25 11:04:45 +0800

    Deploying to Heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const pgp = require('pg-promise')();
 const db = pgp("postgres://postgres:admin@localhost:5432/Dashboard")
+const path = require('path');
 const app = express();
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
