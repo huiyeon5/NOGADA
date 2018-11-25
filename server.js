53e82829366e6893b8de6429f91fc9066b96c952
Huiyeon Kim <gnldus59112@gmail.com>
2018-11-25 11:08:53 +0800

    Added Server Config

--- server.js
+++ server.js
@@ -5,7 +5,18 @@ const path = require('path');
 const app = express();
 var bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
